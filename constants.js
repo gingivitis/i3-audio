d17f3a6d90ebcb0c1c9a4941b0f0140735dac4a8
saliya seneviratne <sseneviratne@thinkful.com>
2015-10-28 12:00:37 -0400

    action symbols created

---
+++ constants.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import keymirror from 'keymirror-symbol'
+
+export
+default keymirror({
+    PLAY_PAUSE: null,
+    PREVIOUS: null,
+    NEXT: null,
+    LOVE: null,
+    HATE: null,
+    CURRENT_SONG: null
+})
